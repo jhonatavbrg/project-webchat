b0fe706ddecf398db394ddaeea0c574736c0fa0f
Jhonata Braga <jhonatavbrg@icloud.com>
2021-12-31 16:13:39 -0300

    add initial files

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-// Faça seu código aqui
+const express = require('express');
+const moment = require('moment');
+require('dotenv').config();
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
